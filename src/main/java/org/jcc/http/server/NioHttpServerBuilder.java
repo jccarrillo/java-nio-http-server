76d93f895fcf1ba5e07a130d963b2b70a1aaabad
JC Carrillo <jccarrillo@acm.org>
2015-12-20 20:53:42 -0500

    SelectionKeyCommand insertion on the server

--- src/main/java/org/jcc/http/server/NioHttpServerBuilder.java
+++ src/main/java/org/jcc/http/server/NioHttpServerBuilder.java
@@ -3,7 +3,6 @@ package org.jcc.http.server;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
-import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -13,7 +12,6 @@ import java.util.LinkedList;
 import java.util.Set;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.Builder;
 
 public class NioHttpServerBuilder implements Builder<HttpServer> {
@@ -33,6 +31,7 @@ public class NioHttpServerBuilder implements Builder<HttpServer> {
 
 		private Selector selector;
 		private ServerSocketChannel serverSocketChannel;
+		private SelectionKeyCommandFactory selectionKeyCommandFactory;
 
 		private NioHttpServer() {
 			super(inetSocketAddress);
@@ -50,6 +49,7 @@ public class NioHttpServerBuilder implements Builder<HttpServer> {
 			this.logger.info("Binded to {}", inetSocketAddress);
 			SelectorProvider selectorProvider = SelectorProvider.provider();
 			this.selector = selectorProvider.openSelector();
+			this.selectionKeyCommandFactory = new SelectionKeyCommandFactory(this.selector);
 			this.logger.info("Selector created");
 			this.serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT);
 		}
@@ -104,14 +104,9 @@ public class NioHttpServerBuilder implements Builder<HttpServer> {
 					if (!selectionKey.isValid()) {
 						continue;
 					}
-					// valid selection key
-					if (selectionKey.isAcceptable()) {
-						doAccept(selectionKey);
-					} else if (selectionKey.isReadable()) {
-						doRead(selectionKey);
-					} else if (selectionKey.isWritable()) {
-						doWrite(selectionKey);
-					}
+					SelectionKeyCommand selectionKeyCommand = this.selectionKeyCommandFactory
+							.getSelectionKeyCommand(selectionKey);
+					selectionKeyCommand.execute(selectionKey);
 				} catch (Exception ex) {
 					StringBuffer buffer = new StringBuffer("Error [");
 					buffer.append(ex.getMessage());
@@ -124,107 +119,6 @@ public class NioHttpServerBuilder implements Builder<HttpServer> {
 				}
 			}
 			this.logger.info("Handled [{}] selection keys", size);
-		}
-
-		/**
-		 * Accept the client, if valid.
-		 * 
-		 * @param selectionKey
-		 * @throws IOException
-		 * @throws ClosedChannelException
-		 */
-		private void doAccept(SelectionKey selectionKey) throws IOException, ClosedChannelException {
-			ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
-			// accept
-			SocketChannel socketChannel = serverSocketChannel.accept();
-			if (socketChannel == null) {
-				this.logger.debug("Disregard client");
-				return;
-			}
-			this.logger.debug("Accepting client");
-			// non blocking
-			socketChannel.configureBlocking(false);
-			// read from the client
-			socketChannel.register(selector, SelectionKey.OP_READ);
-			this.logger.debug("Accepted client");
-		}
-
-		/**
-		 * Reads from the client.
-		 * 
-		 * @param selectionKey
-		 *            the client.
-		 * @throws IOException
-		 * @throws ClosedChannelException
-		 */
-		private void doRead(SelectionKey selectionKey) throws IOException {
-			boolean readable = selectionKey.isReadable();
-			if (!readable) {
-				this.logger.debug("Not readable");
-				return;
-			}
-			this.logger.debug("Reading connection");
-			// get the httpRequestHandler
-			HttpRequestHandler httpRequestHandler = (HttpRequestHandler) selectionKey.attachment();
-			this.logger.debug("Handler [{}]", httpRequestHandler);
-			// create it if it doesn't exist
-			if (httpRequestHandler == null) {
-				this.logger.debug("Creating Handler");
-				httpRequestHandler = new HttpRequestHandlerBuilder().build();
-				selectionKey.attach(httpRequestHandler);
-			}
-			httpRequestHandler = (HttpRequestHandler) selectionKey.attachment();
-			this.logger.debug("Handler [{}]", httpRequestHandler);
-			SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-			// get more data
-			try {
-				httpRequestHandler.read(socketChannel);
-			} catch (IOException e) {
-				// nothing to read
-				socketChannel.close();
-				selectionKey.cancel();
-			}
-			// decode the message
-			String line;
-			while ((line = httpRequestHandler.line()) != null) {
-				// check if we have got everything
-				if (!StringUtils.isEmpty(line)) {
-					continue;
-				}
-				// write the next round
-				selectionKey.interestOps(SelectionKey.OP_WRITE);
-			}
-			this.logger.debug("Read connection");
-		}
-
-		/**
-		 * Writes to the client.
-		 * 
-		 * @param selectionKey
-		 *            the client.
-		 * @throws IOException
-		 */
-		private void doWrite(SelectionKey selectionKey) throws IOException {
-			this.logger.debug("Writing to client");
-			// get the httpRequestHandler
-			HttpRequestHandler httpRequestHandler = (HttpRequestHandler) selectionKey.attachment();
-			if (httpRequestHandler == null) {
-				this.logger.debug("Not writing");
-				throw new IOException("Can't write");
-			}
-			this.logger.debug("Handler [{}]", httpRequestHandler);
-			StringBuilder stringBuilder = httpRequestHandler.getReadLines();
-			String raw = stringBuilder.toString();
-			this.logger.debug("Received:\n{}", raw);
-			// build request
-			HttpRequest request = new HttpRequestBuilder(raw).build();
-			HttpResponse httpResponse = new HttpResponseBuilder().build();
-			httpResponse.setContent("Hello World!".getBytes());
-			SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-			httpRequestHandler.sendResponse(socketChannel, httpResponse);
-			// read the next round.
-			selectionKey.interestOps(SelectionKey.OP_READ);
-			this.logger.debug("Wrote to client");
 		}
 
 		/**
